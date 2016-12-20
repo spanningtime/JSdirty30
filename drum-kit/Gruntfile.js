8a3c0ccaeeaee4a321c4898dc28e05cea11eb65d
William Knight <kniwilliam@gmail.com>
2016-12-19 16:13:24 -0800

    set up grunt live reload and sass

---
+++ drum-kit/Gruntfile.js
@@ -0,0 +1,57 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['app/sass/main.scss'],
+        tasks: ['sass']
+      },
+      html: {
+        files: ['app/index.html']
+      },
+      js: {
+        files: ['app/app.js']
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'app/css/main.css': 'app/sass/main.scss'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 6969,
+          protocol: 'http',
+          hostname: 'localhost',
+          base: 'app',
+          directory: null,
+          open: false,
+          keepalive: true,
+          livereload: true
+        }
+      }
+    },
+    livereload: {
+      options: { livereload: true },
+      files: ['app/css/main.css']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('go', ['sass', 'watch']);
+
+}
