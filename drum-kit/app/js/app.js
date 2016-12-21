35afb2239fc1a8a137f9040121141ec5f21fe00b
William Knight <kniwilliam@gmail.com>
2016-12-21 14:27:03 -0800

    Add transitions to lines when drums are played

--- drum-kit/app/js/app.js
+++ drum-kit/app/js/app.js
@@ -12,8 +12,19 @@ const keyCodes = [
   { keycode: 77, drumType: 'kick'}
 ];
 
-const drums = document.getElementById("drums-container")
+const purpLine = document.getElementById('purp');
+const greenLine = document.getElementById('green');
+const yellowLine = document.getElementById('yellow');
+
+const lines = document.getElementsByClassName('line');
+console.log(lines);
+
+const drums = document.getElementById('drums-container')
 const drumName = document.querySelector('p')
+
+const addDrumName = function() {
+  drumName.classList.add("drumType")
+};
 
 window.addEventListener('keydown', (event) => {
   let target = document.querySelector(`div[data-key="${event.keyCode}"]`)
@@ -24,17 +35,33 @@ window.addEventListener('keydown', (event) => {
   audio.play()
 
   keyCodes.map((el) => {
-    if (event.keyCode == el.keycode) {
+    let kc = event.keyCode;
+    if (kc == el.keycode) {
       drumName.innerHTML = el.drumType.toUpperCase();
     }
-  })
-    target.classList.add("play");
-});
+    if (kc == 89 || kc == 79 || kc == 72) {
+      target.classList.add('cymb-animation');
+      target.classList.add("play");
+      yellowLine.classList.add("line-transition");
+    }
 
+    if (kc == 77 || kc == 78) {
+      purpLine.classList.add("line-transition");
+    }
+
+    if (kc == 85 || kc == 73 || kc == 74 || kc == 75) {
+      greenLine.classList.add("line-transition");
+    }
+    target.classList.add("play");
+  })
+    addDrumName();
+});
 
 drums.addEventListener('click', (event) => {
   console.log(event.target)
   const audio = document.querySelector(`audio[data-key="${event.target.getAttribute('data-key')}"]`);
+
+
 
   if (!audio) return;
   audio.play();
@@ -45,23 +72,39 @@ drums.addEventListener('click', (event) => {
     }
   });
 
+  let dk = event.target.getAttribute('data-key');
+
+  if (dk == 89 || dk == 79 || dk == 72) {
+    event.target.classList.add("cymb-animation");
+    yellowLine.classList.add("line-transition");
+  }
+
+  if (dk == 77 || dk == 78) {
+    purpLine.classList.add("line-transition");
+  }
+
+  if (dk == 85 || dk == 73 || dk == 74 || dk == 75) {
+    greenLine.classList.add("line-transition")
+  };
+
   event.target.classList.add("play");
-  drumName.classList.add("drumType")
+  addDrumName();
 })
 
-const removeDrumTypeTransition = function(event) {
-  drumName.classList.remove('drumType')
+const removeTransition = function(event) {
+  for (let el of lines) {
+    el.classList.remove('line-transition')
+  }
+  event.target.classList.remove('cymb-animation')
+  event.target.classList.remove('play');
+  drumName.classList.remove('drumType');
   drumName.innerHTML = '';
-}
-
-const removePlayTransition = function(event) {
-  event.target.classList.remove('play')
 }
 
 const eachDrum = document.querySelectorAll('.drum');
 
 eachDrum.forEach((drum) => {
-  drum.addEventListener('transitionend', removePlayTransition)
-})
+  drum.addEventListener('transitionend', removeTransition)
+});
 
-drumName.addEventListener('transitionend', removeDrumTypeTransition);
+drumName.addEventListener('transitionend', removeTransition);
