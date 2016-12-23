2cf08b7ac1656ccd166231318bf111fa3a4096ec
William Knight <kniwilliam@gmail.com>
2016-12-23 13:54:41 -0800

    Refactor js logic & start MQs for mobile

--- drum-kit/app/js/app.js
+++ drum-kit/app/js/app.js
@@ -15,57 +15,75 @@ const keyCodes = [
 const purpLine = document.getElementById('purp');
 const greenLine = document.getElementById('green');
 const yellowLine = document.getElementById('yellow');
-
 const lines = document.getElementsByClassName('line');
-console.log(lines);
-
-const drums = document.getElementById('drums-container')
-const drumName = document.querySelector('p')
-
-window.onresize = function() {
-  drumName.innerHTML = "bang on them drums"
-}
+const drums = document.getElementById('drums-container');
+const drumName = document.querySelector('p');
 
 const addDrumName = function() {
   drumName.classList.add("drumType")
 };
 
+window.onresize = function() {
+  drumName.innerHTML = "bang on them drums"
+}
+
+const isYellow = function(code) {
+  console.log(code)
+  if (code == 89 || code == 79 || code == 72) {
+    return true;
+  };
+}
+
+const isPurple = function(code) {
+  console.log(code)
+  if (code == 77 || code == 78) {
+    return true;
+  }
+}
+
+const isGreen = function(code) {
+  console.log(code);
+  if (code == 85 || code == 73 || code == 74 || code == 75) {
+    return true;
+  }
+}
+
+const addClasses = function(code, el) {
+  if (isPurple(code)) {
+    purpLine.classList.add("line-transition");
+  }
+  else if (isGreen(code)) {
+    greenLine.classList.add("line-transition");
+  }
+  else if (isYellow(code)) {
+    yellowLine.classList.add("line-transition");
+    el.classList.add("cymb-animation");
+    el.classList.add("play");
+  }
+}
+
 window.addEventListener('keydown', (event) => {
   let target = document.querySelector(`div[data-key="${event.keyCode}"]`)
-  console.log(target)
+
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 
   if (!audio) return;
   audio.play()
 
   keyCodes.map((el) => {
+    console.log(target);
     let kc = event.keyCode;
     if (kc == el.keycode) {
       drumName.innerHTML = el.drumType.toUpperCase();
     }
-    if (kc == 89 || kc == 79 || kc == 72) {
-      target.classList.add('cymb-animation');
-      target.classList.add("play");
-      yellowLine.classList.add("line-transition");
-    }
-
-    if (kc == 77 || kc == 78) {
-      purpLine.classList.add("line-transition");
-    }
-
-    if (kc == 85 || kc == 73 || kc == 74 || kc == 75) {
-      greenLine.classList.add("line-transition");
-    }
+    addClasses(kc, target);
     target.classList.add("play");
-  })
+  });
     addDrumName();
 });
 
 drums.addEventListener('click', (event) => {
-  console.log(event.target)
   const audio = document.querySelector(`audio[data-key="${event.target.getAttribute('data-key')}"]`);
-
-
 
   if (!audio) return;
   audio.play();
@@ -78,19 +96,7 @@ drums.addEventListener('click', (event) => {
 
   let dk = event.target.getAttribute('data-key');
 
-  if (dk == 89 || dk == 79 || dk == 72) {
-    event.target.classList.add("cymb-animation");
-    yellowLine.classList.add("line-transition");
-  }
-
-  if (dk == 77 || dk == 78) {
-    purpLine.classList.add("line-transition");
-  }
-
-  if (dk == 85 || dk == 73 || dk == 74 || dk == 75) {
-    greenLine.classList.add("line-transition")
-  };
-
+  addClasses(dk, event.target)
   event.target.classList.add("play");
   addDrumName();
 })
@@ -107,8 +113,12 @@ const removeTransition = function(event) {
 
 const eachDrum = document.querySelectorAll('.drum');
 
+const endTransition = function(element) {
+  element.addEventListener('transitionend', removeTransition);
+}
+
 eachDrum.forEach((drum) => {
-  drum.addEventListener('transitionend', removeTransition)
+  endTransition(drum)
 });
 
-drumName.addEventListener('transitionend', removeTransition);
+endTransition(drumName);
